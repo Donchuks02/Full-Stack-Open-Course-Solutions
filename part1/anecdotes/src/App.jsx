17131240140a228c2a6a3f498f6c4edeb34115e6
donchuks <chukwudidavid02@gmail.com>
2025-10-04 16:22:22 +0100

    added solution for part1-anecdotes tasks

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.onClick}>{props.text}</button>
+)
+
+
+
+const App = () => {
+const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [voteCount, setVoteCount] = useState(Array(anecdotes.length).fill(0))
+
+  const getRandomAnecdotes = () => {
+    return Math.floor(Math.random() * anecdotes.length)
+  }
+
+  const voteCounter = () => {
+    const newVotes = [...voteCount];
+    newVotes[selected] += 1
+    setVoteCount(newVotes)
+  }
+
+
+
+  const findMaxVotes = () => {
+    const maxVotes = Math.max(...voteCount);
+    const indexOfMax = voteCount.indexOf(maxVotes);
+    return indexOfMax;
+  }
+
+  const mostVotedAnecdoteIndex = findMaxVotes();
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+
+      {anecdotes[selected]}<br />
+      has {voteCount[selected]} votes
+      <br />
+
+      <Button onClick={voteCounter} text="vote"/>
+      <Button onClick={() => setSelected(getRandomAnecdotes())} text="next anecdote" />
+
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[mostVotedAnecdoteIndex]}<br />
+      has {voteCount[mostVotedAnecdoteIndex]} votes
+      <br />
+
+    </div>
+  )
+}
+
+export default App
