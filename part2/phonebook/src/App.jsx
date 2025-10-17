de951272090bf0f293a7377fe2fe9f2c4e84f69b
donchuks <chukwudidavid02@gmail.com>
2025-10-17 21:37:17 +0100

    Solved part2  phonebook step3 exercise

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567', }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNames = (event) => {
     event.preventDefault()
@@ -16,15 +17,24 @@ const App = () => {
     }
     const namesObject = {
       name: newName,
+      number : newNumber,
+      
     }
-    setPersons(persons.concat(namesObject))
+    
+    setPersons(persons.concat(namesObject))    
     setNewName('')
+    setNewNumber('')
+
     
     
   }
 
   const handleNamesChange = (event) => {
     setNewName(event.target.value)
+    
+  }
+  const handleNumbersChange = (event) => {
+    setNewNumber(event.target.value)
     
   }
 
@@ -35,17 +45,15 @@ const App = () => {
      
       <h2>Phonebook</h2>
       <form onSubmit={addNames}>
-        <div>
-          name: <input value={newName} onChange={handleNamesChange} />
-
-        </div>
+        <div>name: <input value={newName} onChange={handleNamesChange} /></div>
+        <div>number : <input value={newNumber} onChange={handleNumbersChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map(person => (<li key={person.name}>{person.name}</li>))}
+      {persons.map(person => (<li key={person.name}>{person.name} {person.number}</li>))}
       </ul>
     </div>
   )
