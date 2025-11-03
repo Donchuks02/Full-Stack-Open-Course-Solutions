f197ba874f95ff4979c7ff5955e655d27a66d5d5
donchuks <chukwudidavid02@gmail.com>
2025-11-03 09:30:54 +0100

    Solved part2 phonebook step11(handled a error message if a person is added)  exercise

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,12 +5,26 @@ import Persons from './components/Persons'
 import axios from 'axios'
 import phonebookServices from './services/phonebook'
 
+
+const Notification = ({message}) => {
+  if(message ===  null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchPerson] = useState('')
   const [searchedPerson, setSearchedPerson] = useState(persons)
+  const [errorMessage, setErrorMessage] = useState('')
 
   useEffect(() => {
 
@@ -46,6 +60,12 @@ const App = () => {
           setSearchedPerson(searchedPerson.map(p => p.id === returnedPerson.id ? returnedPerson : p))
           setNewName('')
           setNewNumber('')
+          
+        }).catch(error => {
+          setErrorMessage(`Information of ${existingPerson.name} has been removed from server`)
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
         })
 
       }else{
@@ -69,6 +89,12 @@ const App = () => {
       setNewName('')
       setNewNumber('')
       setSearchedPerson(persons.concat(returnedPhoneBook))
+      setErrorMessage(
+        `Added ${namesObject.name}`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      },5000)
 
     })
 
@@ -107,6 +133,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter searchName={searchName} handleSearchName={handleSearchName} />
       <h3>Add a new</h3>
       <PersonForm 
