940af9862fffcb7f6376531a8a5890583efe5b20
Chuks <chukwudidavid02@gmail.com>
2026-08-27 15:48:36 +0100

    added CORs

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const baseUrl = 'https://full-stack-open-course-solutions.onrender.com/'
+const baseUrl = 'https://full-stack-open-course-solutions.onrender.com/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
